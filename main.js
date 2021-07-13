d63322657812f2bb69c46ab20a0270ddeb1bb6f9
qnlex <87347082+qnlex@users.noreply.github.com>
2021-07-12 21:41:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-keysNum = [];
+
+pianoMade = false;
+
+function makePiano() {
+  pianoMade = true;
+  keysAmount = Math.round(parseInt(document.getElementById('keyNum').value));
+  if (keysAmount < 5)
+    {
+        alert("There must be at least 5 keys");
+        pianoMade = false;
+        return false;
+    }
+}
